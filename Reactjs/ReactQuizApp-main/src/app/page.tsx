81a2033e4fc13b66e7ab5060c8871f418df13004
raxeraditya <brahmaditya@outlook.com>
2026-07-08 07:00:55 +0530

    initial commit

---
+++ Reactjs/ReactQuizApp-main/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FullScreenPage from "./Components/FullScreen";
+
+const page = () => {
+  return (
+    <div className="pt-10 px-24 w-full">
+      <FullScreenPage />
+    </div>
+  );
+};
+
+export default page;
